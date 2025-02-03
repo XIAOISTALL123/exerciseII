93ff079c9b6c3d2d1372251c504548a9a3d8762c
boyadik69 <4Zhiraphael@gmail.com>
2025-02-03 22:28:26 +0800

    Debugged gcd, get_numerator, get_denominator, get_fraction. Added static method euclidean, helper function for gcd

--- Fraction.py
+++ Fraction.py
@@ -20,17 +20,46 @@ class Fraction(object):
         if denominator == 0:
             raise ZeroDivisionError
 
-    def gcd(a, b):
-        if b == 0:
-            return abs(a)
+    @staticmethod
+    def euclidean(a, b):
+        if int(b) == 0:
+            return abs(int(a))
         else:
-            return gcd(b, a % b)
+            return Fraction.euclidean(int(b), int(a) % int(b))
+
+    @staticmethod
+    def gcd(a, b):
+        if int(b) == 0 or int(a) == 0:
+            return 0
+        else:
+            return Fraction.euclidean(int(a), int(b))
 
     def get_numerator(self):
-        return str(self.numerator//gcd(self.numerator, self.denominator))
+        if Fraction.gcd(self.numerator, self.denominator) != 0:
+            return str(int(self.numerator)//Fraction.gcd(self.numerator, self.denominator))
+        else:
+            return '0'
 
     def get_denominator(self):
-        return str(self.denominator//gcd(self.numerator, self.denominator))
+        if Fraction.gcd(self.numerator, self.denominator) != 0:
+            return str(int(self.denominator)//Fraction.gcd(self.numerator, self.denominator))
+        else:
+            return '0'
 
     def get_fraction(self):
-        return self.get_numerator() + '/' + self.get_denominator()
+        try:
+            if int(self.denominator) == 0 or int(self.numerator) == 0:
+                return '0'
+
+            if int(self.denominator) < 0:
+                if int(self.numerator) < 0:
+                    return self.get_numerator()[1:] + '/' + self.get_denominator()[1:]
+                else:
+                    return '-' + self.get_numerator() + '/' + self.get_denominator()[1:]
+
+            elif int(self.denominator) == 0 or int(self.numerator) == 0:
+                return '0'
+            else:
+                return self.get_numerator() + '/' + self.get_denominator()
+        except:
+            return '0'
