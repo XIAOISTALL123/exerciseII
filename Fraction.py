b5d578e8fdffd4259f0ee4cd7d61f9bed618c721
boyadik69 <168244480+XIAOISTALL123@users.noreply.github.com>
2025-02-03 23:51:17 +0800

    Update Fraction.py
    Fixed implementation for gcd, added euclidean static method

--- Fraction.py
+++ Fraction.py
@@ -20,6 +20,13 @@ class Fraction(object):
         if denominator == 0:
             raise ZeroDivisionError
 
+    @staticmethod
+    def euclidean(a, b):
+        if int(b) == 0:
+            return abs(int(a))
+        else:
+            return Fraction.euclidean(int(b), int(a) % int(b))
+    @staticmethod    
     def gcd(a, b):
         if int(b) == 0 or int(a) == 0:
             return 0
@@ -54,3 +61,4 @@ class Fraction(object):
             else:
                 return self.get_numerator() + '/' + self.get_denominator()
         except:
+            return '0'
