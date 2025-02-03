0a51ba83be76b682ec3b77914f68fe56dd4788b1
JavaDucks <jedale.sabio@student.ateneo.edu>
2025-02-03 17:39:29 +0800

    get_denominator function is now complete

--- Fraction.py
+++ Fraction.py
@@ -27,12 +27,10 @@ class Fraction(object):
             return gcd(b, a % b)
 
     def get_numerator(self):
-        #TODO
-        pass
+        return str(self.numerator//gcd(self.numerator, self.denominator))
 
     def get_denominator(self):
-        #TODO
-        pass
+        return str(self.denominator//gcd(self.numerator, self.denominator))
 
     def get_fraction(self):
         #TODO
